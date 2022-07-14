11b398951e4611ddf8233d1a745c4108bbb15b2b
mahdiyar marvi <mahdiyar472@gmail.com>
2022-07-15 00:05:49 +0430

    fix bugs

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import UsersApp from "./components/UsersApp/UsersApp";
-import DropZone from "./components/dropZone/DropZone";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
